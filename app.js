7443e720b53ef9774f31093e9c82d873d5060160
monxchrome <inacheat@gmail.com>
2023-03-02 17:20:44 +0100

    api

--- app.js
+++ app.js
@@ -1,35 +1,89 @@
 const fs = require('fs');
 const path = require('path');
+const express = require('express');
+const users = require('./main/db.js')
+
+const app = express();
+const PORT = 5100;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(5100, () => {
+    console.log(`Server has started on port: ${PORT}`)
+});
 
 // fs.mkdir(path.join('main'), (err) => {
 //     if (err) throw new Error(err.message);
 // })
 
-// for (let i = 0; i < 5; i++) {
-//     fs.mkdir(path.join('main', `dir${i}`), (err) => {
-//         if (err) throw new Error(err.message);
-//     })
-// }
+// Винести базу даних в json.file, при створенні записувати туда нових юзерів через fs
+// При створенні валідацію на імія і вік, імя повинно бути більше 2 символів, вік – не менше нуля
+// На гет, пут, деліт юзерів перевірити чи такий юзер є
 
-// for (let i = 0; i < 3; i++) {
-//     fs.writeFile(path.join('main', `javascript${i}.js`), '', (err) => {
-//         if (err) throw new Error(err.message)
-//     })
-// }
-//
-// for (let i = 3; i < 5; i++) {
-//     fs.writeFile(path.join('main', `text${i}.txt`), '', (err) => {
-//         if (err) throw new Error(err.message)
-//     })
-// }
+// fs.writeFile(path.join('main', 'db.js'), '', (err) => {
+//     if (err) throw new Error(err.message);
+// })
 
-fs.readdir(path.join('main'), {withFileTypes: true},(err, data)=>{
-  if (err) throw new Error();
-  data.forEach(data => {
-      if (data.isDirectory()) {
-          console.log('FOLDER: ', data.name)
-      } if (data.isFile()) {
-          console.log('FILE: ', data.name)
-      }
-  })
+app.get('/users', (req, res) => {
+    res.status(200).json(users);
+});
+
+app.get('/users/:userID', (req, res) => {
+    const { userID } = req.params;
+
+    if (+userID > users.users.length) {
+        res.status(404).json({
+            message: 'Not found'
+        })
+    } else {
+        const user = users.users[+userID];
+        res.status(200).json(user)
+    }
+});
+
+app.post('/users', (req, res) => {
+    const body = req.body
+    if (body.name.length > 2 && body.age >= 0) {
+        users.users.push(body)
+        res.status(200).json({
+            message: 'User Created.'
+        })
+    } else {
+        res.status(418).json({
+            message: 'ERROR! User name must be > 2 and User age must be >= 0'
+        })
+    }
+})
+
+app.put('/users/:userID', (req, res) => {
+    const { userID } = req.params;
+    const updatedUser = req.body;
+
+    if (+userID > users.users.length) {
+        res.status(404).json({
+            message: 'Not found'
+        })
+    } else {
+        users.users[+userID] = updatedUser;
+        res.status(200).json({
+            message: 'User is updated',
+            data: users.users[+userID]
+        })
+    }
+})
+
+app.delete('/users/:userID', (req, res) =>  {
+    const { userID } = req.params;
+
+    if (+userID > users.users.length) {
+        res.status(404).json({
+            message: 'Not found'
+        })
+    } else {
+        users.users.splice(+userID, 1);
+        res.status(200).json({
+            message: 'User has been deleted.'
+        })
+    }
 })
---
+++ main/db.js
@@ -0,0 +1,31 @@
+const users = [
+    {
+        name: 'Stefan',
+        age: 17,
+        gender: 'male'
+    },
+    {
+        name: 'Kristina',
+        age: 15,
+        gender: 'female'
+    },
+    {
+        name: 'Anya',
+        age: 25,
+        gender: 'female'
+    },
+    {
+        name: 'Elizaveta',
+        age: 16,
+        gender: 'female'
+    },
+    {
+        name: 'Kokos',
+        age: 20000,
+        gender: 'mixed'
+    }
+]
+
+module.exports = {
+    users
+}
